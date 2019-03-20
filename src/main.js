94a9c1ffdaf8cb2d37c721ab2114e64c58b0f30d
Blake Loring <blake@parsed.uk>
2019-03-20 14:54:23 +0000

    Bumpup

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ const cookieParser = require('cookie-parser')
 const exphbs = require('express-handlebars');
 const bcrypt = require('bcrypt');
 
-db.exec('CREATE TABLE IF NOT EXISTS users (id PRIMARY KEY, name TEXT, password TEXT)');
-db.exec('CREATE TABLE IF NOT EXISTS sessions(id PRIMARY KEY, uid KEY)');
-db.exec('CREATE TABLE IF NOT EXISTS characters (id PRIMARY KEY, owner KEY, name TEXT, details TEXT)');
+db.exec('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY ASC, name TEXT, password TEXT)');
+db.exec('CREATE TABLE IF NOT EXISTS sessions(id INTEGER PRIMARY KEY ASC, uid INTEGER, FOREIGN KEY(uid) REFERENCES users(id))');
+db.exec('CREATE TABLE IF NOT EXISTS characters (id INTEGER PRIMARY KEY, owner INTEGER, name TEXT, details TEXT, FOREIGN KEY(owner) REFERENCES users(id))');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -18,7 +18,19 @@ app.use(cookieParser());
 app.use(express.static('./static/'));
 
 function createSession(userid) {
-  return db.prepare('INSERT INTO sessions(uid) VALUES(?)').run(userid).id;
+  return db.prepare('INSERT INTO sessions(uid) VALUES(?)').run(userid).lastInsertRowid;
+}
+
+function validSession(sid) {
+  if (!sid) {
+    return false;
+  }
+
+  const session = db.prepare('SELECT * FROM sessions WHERE id=?').get(sid);
+  
+  return session ? {
+    uid: session.uid
+  } : undefined;
 }
 
 app.get('/', function(req, res) {
@@ -27,7 +39,7 @@ app.get('/', function(req, res) {
   if (!req.cookies.sid) {
     res.redirect('/login');
   } else {
-    res.render('main');
+    res.redirect('/main');
   }
 });
 
@@ -43,14 +55,13 @@ app.post('/login', (req, res) => {
   const targetPassword = req.body.password;
 
   const row = db.prepare('SELECT id, name, password FROM users WHERE name=? LIMIT 1').get(targetUsername);
-  let user = undefined;
 
   if (row) {
     console.log('[+] Login found user');
     bcrypt.compare(targetPassword, row.password, (err, correct) => {
       if (correct) {
         console.log('[+] Logged In ' + targetUsername);
-        res.cookie('sid', createSession(user.id), { maxAge: 900000 });
+        res.cookie('sid', createSession(row.id), { maxAge: 900000 });
         res.redirect('/main');
       } else {
         console.log('[+] Bad password on: ' + targetUsername);
@@ -69,8 +80,6 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
 
-  console.log('Registering');
-
   const targetUsername = req.body.username;
   const targetPassword = req.body.password;
 
@@ -78,7 +87,6 @@ app.post('/register', (req, res) => {
   let user = undefined;
 
   if (!row) {
-    console.log('Registering');
     bcrypt.hash(targetPassword, 16, function(err, hash) {
       if (err) {
         console.log('Bcrypt hash error');
@@ -95,10 +103,67 @@ app.post('/register', (req, res) => {
   }
 });
 
+app.get('/main', function(req, res) {
 
-app.get('/characters', function(req, res) {
-  const characters = db.prepare('SELECT * FROM characters').get();
-  res.send(JSON.stringify(characters));
+  if (!validSession(req.cookies.sid)) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('base');
+});
+
+app.get('/new_character', function(req, res) {
+  res.render('new_character');
+});
+
+app.get('/view_character', function(req, res) {
+  res.render('view_character');
+});
+
+app.get('/character', function(req, res) {
+
+  const session = validSession(req.cookies.sid);
+
+  if (!session) {
+    res.send(JSON.stringify({error: 'Bad Session'}));
+    return;
+  }
+  if (req.query.id) {
+    res.send(db.prepare('SELECT * FROM characters WHERE owner=? AND id=?').get(session.uid, req.query.id));
+  } else {
+    res.send(db.prepare('SELECT * FROM characters WHERE owner=?').all(session.uid));
+  }
+});
+
+app.post('/character', function(req, res) {
+  const session = validSession(req.cookies.sid);
+  
+  if (!session) {
+    res.send({error: 'Bad Session'});
+    return;
+  }
+
+  if (!req.body.name) {
+    res.send({error: 'Bad Name'});
+    return;
+  }
+
+  let query = null;
+
+  if (req.body.id) {
+    //Update
+    query = db.prepare('UPDATE characters SET name=?, details=? WHERE id=? AND owner=?')
+      .run(req.body.name, JSON.stringify(req.body), req.body.id, session.uid);
+  } else {
+    //Insert
+    query = db.prepare('INSERT INTO characters(owner, name, details) VALUES(?, ?, ?)')
+      .run(session.uid, req.body.name, JSON.stringify(req.body));
+  }
+
+  console.log('[+]' + req.body.name);
+
+  res.send({ id: query.lastInsertRowid });
 });
 
 var server = app.listen(8080, function () {
--- static/js/main.js
+++ static/js/main.js
@@ -1,79 +1,26 @@
-function GenerateStats(character) {
-  let srcTable = '<h3>Stats</h3><table class="table"><tbody>';
-
-  let stats = character.data.stats;
-
-  function addStat(name, val) {
-    srcTable += `<tr><td>${name}</td><td>${val}</td><td>${Math.floor((val - 10) / 2)}</td></tr>`;
-  }
-
-  for (stat in stats) {
-    addStat(stat, stats[stat]);
-  }
-
-  srcTable += '</tbody></table>';
-  return srcTable;
-}
-
-function GenerateInventory(character) {
-  let srcTable = '<h3>Inventory</h3><table class="table"><tbody>';
-
-  let items = character.data.inventory;
-
-  function addItem(name, desc, mods) {
-    srcTable += `<tr><td>${name}</td><td>${desc}</td><td>${mods}</td></tr>`;
-  }
-
-  items.forEach(item => {
-    addItem(item.name, item.desc, item.mods);
-  });
-
-  srcTable += '</tbody></table>';
-  return srcTable;
-}
-
-function AddNode(src) {
-  let newDiv = document.createElement('div');
-  newDiv.innerHTML = src;
-  document.body.appendChild(newDiv);
-  return newDiv;
-}
-
-function SelectCharacter(character) {
-
-  while (document.body.children.length) {
-    document.body.removeChild(document.body.children[0]);
-  }
-
-  let nameNode = document.createElement('h3');
-  nameNode.innerHTML = character.name;
-  document.body.appendChild(nameNode);
-
-  AddNode(GenerateStats(character));   
-  AddNode(GenerateInventory(character));
-  AddNode(`<h3>Racials</h3> ${character.data.racials}`);
-  AddNode(`<h3>Languages</h3> ${character.data.languages}`);
-
-  let bio = document.createElement('p');
-  bio.innerHTML = character.data.bio;
-  document.body.appendCHild(bio);
-}
-
-document.onreadystatechange = function() {
-  SelectCharacter({
-    name: 'Lawnmower',
-    data: {
-      stats: {
-        'STR': 15,
-        'CON': 13,
-        'CHR': 13,
-        'DEX': 13,
-        'WIS': 11,
-        'INT': 11
-      },
-      racials: "Dark Vision, Gnomish Cunning. Advantage Wisdom Int Charisma",
-      languages: "All: Gnomish Reads: Dwarf",
-      inventory: [ {name: "Mr Scissors", desc: "Glorious Scissors", mods: "1D6 1H 1D8 2H"} ]
+class CharacterPart { 
+  constructor(obj) {
+    this.core = obj;
+    this._is_part = true; 
+   for (let field in this.core) {
+      if (typeof(this.core[field]) === "object" && this.core[field]._is_part) {
+        this.core[field] = new CharacterPart(this.core[field]);
+      }
     }
-  });
+  }
+
+  field(id, val) {
+    if (typeof(val) === 'undefined') {
+      return this.core[id];
+    } else {
+      this.core[id] = val;
+    }
+  }
+
+  area(id) {
+    if (!this.core[id]) {
+      this.core[id] = new CharacterPart({});
+    }
+    return this.core[id];
+  }
 }
